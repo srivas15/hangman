71d97421b78e445f15f0900257394a5638a11f5c
srivas15 <srivas15@purdue.edu>
2015-08-12 20:03:07 +0530

    minor changes for heroku

--- run5.py
+++ run5.py
@@ -10,7 +10,7 @@ import random
 alphabetList = ['-', 'a', 'e', 'i', 'o', 'u', ' ', '0', '1', '2','3','4','5','6','7','8','9']
 chances = 0
 mainList = []
-main = ""
+main = "aaaaaaaa"
 
 @app.route('/')
 @app.route('/index')
